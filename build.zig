4a0fc77b44b5cd6df7213bf54990ca816d6598e8
Frost-Phoenix <67cyril6767@gmail.com>
2025-08-15 16:57:03 +0200

    feat: add Single Step Tests runner

--- build.zig
+++ build.zig
@@ -17,15 +17,28 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
+    const exe_test_sst = b.addExecutable(.{
+        .name = "zig80_test_sst",
+        .root_source_file = b.path("src/test_sst.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
+    const run_cmd_test_sst = b.addRunArtifact(exe_test_sst);
+
     if (b.args) |args| {
         run_cmd.addArgs(args);
+        run_cmd_test_sst.addArgs(args);
     }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const run_step_test_sst = b.step("test-sst", "Run Single Step Tests");
+    run_step_test_sst.dependOn(&run_cmd_test_sst.step);
 
     const exe_unit_tests = b.addTest(.{
         .root_module = exe_mod,
---
+++ src/test_sst.zig
@@ -0,0 +1,192 @@
+// ********** imports ********** //
+
+const Z80 = @import("Z80.zig");
+
+const std = @import("std");
+
+const log = std.log;
+const json = std.json;
+
+const Allocator = std.mem.Allocator;
+
+const expectEqual = std.testing.expectEqual;
+
+// ********** types ********** //
+
+const TestConfig = struct {
+    name: []const u8,
+    initial: struct {
+        pc: u16,
+        sp: u16,
+        a: u8,
+        f: u8,
+        b: u8,
+        c: u8,
+        d: u8,
+        e: u8,
+        h: u8,
+        l: u8,
+        i: u8,
+        r: u8,
+        wz: u16,
+        ix: u16,
+        iy: u16,
+        af_: u16,
+        bc_: u16,
+        de_: u16,
+        hl_: u16,
+        iff1: u1,
+        iff2: u1,
+        ram: [][2]u16,
+        // ei
+        // im
+        // p
+        // q
+    },
+    final: struct {
+        pc: u16,
+        sp: u16,
+        a: u8,
+        f: u8,
+        b: u8,
+        c: u8,
+        d: u8,
+        e: u8,
+        h: u8,
+        l: u8,
+        i: u8,
+        r: u8,
+        wz: u16,
+        ix: u16,
+        iy: u16,
+        af_: u16,
+        bc_: u16,
+        de_: u16,
+        hl_: u16,
+        iff1: u1,
+        iff2: u1,
+        ram: [][2]u16,
+        // ei
+        // im
+        // p
+        // q
+    },
+    // cycles
+};
+
+// ********** private functions ********** //
+
+fn parseTestConfig(allocator: Allocator, path: []const u8) !json.Parsed(TestConfig) {
+    const file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
+    defer file.close();
+
+    var buffered = std.io.bufferedReader(file.reader());
+    var reader = std.json.reader(allocator, buffered.reader());
+    defer reader.deinit();
+
+    const parsed = try std.json.parseFromTokenSource(TestConfig, allocator, &reader, .{
+        .allocate = .alloc_always,
+        .ignore_unknown_fields = true,
+    });
+
+    return parsed;
+}
+
+fn setZ80State(z: *Z80, config: TestConfig) void {
+    const init = config.initial;
+
+    z.pc = init.pc;
+    z.sp = init.sp;
+
+    z.a = init.a;
+    z.f.b = init.f;
+
+    z.b = init.b;
+    z.c = init.c;
+    z.d = init.d;
+    z.e = init.e;
+    z.h = init.h;
+    z.l = init.l;
+
+    z.af_ = init.af_;
+    z.bc_ = init.bc_;
+    z.de_ = init.de_;
+    z.hl_ = init.hl_;
+
+    z.ix = init.ix;
+    z.iy = init.iy;
+
+    z.i = init.i;
+    z.r = init.r;
+
+    z.iff1 = init.iff1 == 1;
+    z.iff2 = init.iff2 == 1;
+
+    for (init.ram) |pair| {
+        const addr = pair[0];
+        const val = pair[1];
+
+        z.memory[addr] = @truncate(val);
+    }
+}
+
+fn expectState(z: *Z80, config: TestConfig) !void {
+    const fin = config.final;
+
+    try expectEqual(fin.pc, z.pc);
+    try expectEqual(fin.sp, z.sp);
+
+    try expectEqual(fin.a, z.a);
+    try expectEqual(fin.f, z.f.b);
+    try expectEqual(fin.b, z.b);
+    try expectEqual(fin.c, z.c);
+    try expectEqual(fin.d, z.d);
+    try expectEqual(fin.e, z.e);
+    try expectEqual(fin.h, z.h);
+    try expectEqual(fin.l, z.l);
+
+    try expectEqual(fin.af_, z.af_);
+    try expectEqual(fin.bc_, z.bc_);
+    try expectEqual(fin.de_, z.de_);
+    try expectEqual(fin.hl_, z.hl_);
+
+    try expectEqual(fin.ix, z.ix);
+    try expectEqual(fin.iy, z.iy);
+
+    try expectEqual(fin.i, z.i);
+    // try expectEqual(fin.r, z.r); // Not implemented yet
+
+    try expectEqual(fin.iff1 == 1, z.iff1);
+    try expectEqual(fin.iff2 == 1, z.iff2);
+
+    for (fin.ram) |pair| {
+        const addr = pair[0];
+        const val = pair[1];
+
+        try expectEqual(@as(u8, @truncate(val)), z.memory[addr]);
+    }
+}
+
+// ********** public functions ********** //
+
+pub fn main() !void {
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    log.info("Z80 Single Step Tests", .{});
+
+    var z: Z80 = .init();
+
+    const path = "./tests/test.json";
+
+    const parsed_config = try parseTestConfig(allocator, path);
+    defer parsed_config.deinit();
+    const config = parsed_config.value;
+
+    setZ80State(&z, config);
+    z.step();
+    try expectState(&z, config);
+
+    log.info("Test \"{s}\": passed", .{config.name});
+}
