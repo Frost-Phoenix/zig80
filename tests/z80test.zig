475ad763a1a1d08339d1005fc252346225ff2da0
Frost-Phoenix <67cyril6767@gmail.com>
2026-04-22 10:17:50 +0200

    feat: update z80test test runner to zig 0.16.0

--- tests/z80test.zig
+++ tests/z80test.zig
@@ -6,6 +6,7 @@ const std = @import("std");
 
 const log = std.log;
 
+const Io = std.Io;
 const Allocator = std.mem.Allocator;
 
 // ********** global var ********** //
@@ -21,10 +22,6 @@ var z: Z80 = .init(.{
 var memory: [65536]u8 = @splat(0);
 
 const rom_extention = ".tap";
-
-var stdout_buffer: [1024]u8 = undefined;
-var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-const stdout = &stdout_writer.interface;
 
 // ********** private functions ********** //
 
@@ -42,25 +39,25 @@ fn ioRead(_: u16) u8 {
 
 fn ioWrite(_: u16, _: u8) void {}
 
-fn loadRom(allocator: Allocator, rom_name: []const u8) !void {
+fn loadRom(io: Io, alloc: Allocator, rom_name: []const u8) !void {
     const base_path = "./tests/roms/";
 
-    const rom_path = try std.mem.concat(allocator, u8, &[_][]const u8{
+    const rom_path = try std.mem.concat(alloc, u8, &[_][]const u8{
         base_path,
         rom_name,
         rom_extention,
     });
-    defer allocator.free(rom_path);
+    defer alloc.free(rom_path);
 
-    const file = try std.fs.cwd().openFile(rom_path, .{ .mode = .read_only });
-    defer file.close();
+    const file = try std.Io.Dir.cwd().openFile(io, rom_path, .{ .mode = .read_only });
+    defer file.close(io);
 
     var buffer: [1024]u8 = undefined;
-    var file_reader = file.reader(&buffer);
+    var file_reader = file.reader(io, &buffer);
     const reader = &file_reader.interface;
 
-    const rom_data = try reader.allocRemaining(allocator, .unlimited);
-    defer allocator.free(rom_data);
+    const rom_data = try reader.allocRemaining(alloc, .unlimited);
+    defer alloc.free(rom_data);
 
     const skip = 0x5B;
 
@@ -72,15 +69,15 @@ fn loadRom(allocator: Allocator, rom_name: []const u8) !void {
     memory[0x1601] = 0xC9;
 }
 
-fn runTest(allocaor: Allocator, rom_name: []const u8) !void {
+fn runTest(io: Io, allocaor: Allocator, stdout: *Io.Writer, rom_name: []const u8) !void {
     z.reset();
     z.pc = start_addr;
 
-    try loadRom(allocaor, rom_name);
+    try loadRom(io, allocaor, rom_name);
 
     log.info("Running {s}{s}\n", .{ rom_name, rom_extention });
 
-    var timer: std.time.Timer = try .start();
+    const timer: std.Io.Timestamp = .now(io, .awake);
 
     while (z.pc != 0x0000) {
         z.step();
@@ -98,7 +95,8 @@ fn runTest(allocaor: Allocator, rom_name: []const u8) !void {
         }
     }
 
-    const test_time: f128 = @as(f128, @floatFromInt(timer.read())) / 1_000_000_000.0;
+    const duration = Io.Timestamp.untilNow(timer, io, .awake);
+    const test_time: f128 = @as(f128, @floatFromInt(duration.toNanoseconds())) / 1_000_000_000.0;
 
     try stdout.print("\n", .{});
     try stdout.flush();
@@ -113,42 +111,44 @@ fn runTest(allocaor: Allocator, rom_name: []const u8) !void {
 
 // ********** public functions ********** //
 
-pub fn main() !void {
-    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+pub fn main(init: std.process.Init) !void {
+    const alloc = init.gpa;
+    const io = init.io;
+
+    const args = try init.minimal.args.toSlice(init.arena.allocator());
+
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(io, &stdout_buffer);
+    const stdout = &stdout_writer.interface;
 
     log.info("{s}Z80 z80test Tests{s}", .{ "\x1b[1m", "\x1b[0m" });
 
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
     if (args.len == 3 and std.mem.eql(u8, args[1], "--run")) {
-        if (std.mem.eql(u8, args[2], "z80doc")) try runTest(allocator, "z80doc");
-        if (std.mem.eql(u8, args[2], "z80full")) try runTest(allocator, "z80full");
-        if (std.mem.eql(u8, args[2], "z80docflags")) try runTest(allocator, "z80docflags");
-        if (std.mem.eql(u8, args[2], "z80flags")) try runTest(allocator, "z80flags");
-        if (std.mem.eql(u8, args[2], "z80ccf")) try runTest(allocator, "z80ccf");
-        if (std.mem.eql(u8, args[2], "z80memptr")) try runTest(allocator, "z80memptr");
+        if (std.mem.eql(u8, args[2], "z80doc")) try runTest(io, alloc, stdout, "z80doc");
+        if (std.mem.eql(u8, args[2], "z80full")) try runTest(io, alloc, stdout, "z80full");
+        if (std.mem.eql(u8, args[2], "z80docflags")) try runTest(io, alloc, stdout, "z80docflags");
+        if (std.mem.eql(u8, args[2], "z80flags")) try runTest(io, alloc, stdout, "z80flags");
+        if (std.mem.eql(u8, args[2], "z80ccf")) try runTest(io, alloc, stdout, "z80ccf");
+        if (std.mem.eql(u8, args[2], "z80memptr")) try runTest(io, alloc, stdout, "z80memptr");
     } else if (args.len != 1) {
         @panic("unknown arg");
     } else {
         // run all
-        try runTest(allocator, "z80doc");
+        try runTest(io, alloc, stdout, "z80doc");
         try stdout.print("\n", .{});
         try stdout.flush();
-        try runTest(allocator, "z80full");
+        try runTest(io, alloc, stdout, "z80full");
         try stdout.print("\n", .{});
         try stdout.flush();
-        try runTest(allocator, "z80docflags");
+        try runTest(io, alloc, stdout, "z80docflags");
         try stdout.print("\n", .{});
         try stdout.flush();
-        try runTest(allocator, "z80flags");
+        try runTest(io, alloc, stdout, "z80flags");
         try stdout.print("\n", .{});
         try stdout.flush();
-        try runTest(allocator, "z80ccf");
+        try runTest(io, alloc, stdout, "z80ccf");
         try stdout.print("\n", .{});
         try stdout.flush();
-        try runTest(allocator, "z80memptr");
+        try runTest(io, alloc, stdout, "z80memptr");
     }
 }
