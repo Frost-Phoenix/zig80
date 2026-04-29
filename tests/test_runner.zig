8e76bdcb44eb3f16275df31fd83039a8fdd9ca4c
Frost-Phoenix <67cyril6767@gmail.com>
2026-04-29 15:10:38 +0200

    test: update cli

--- tests/test_runner.zig
+++ tests/test_runner.zig
@@ -17,11 +17,14 @@ const BOLD = "\x1b[1m";
 const RESET = "\x1b[0m";
 const YELLOW = "\x1b[33m";
 
+const SUCCESS = 0;
+const FAILURE = 1;
+
 // ********** private functions ********** //
 
-fn printUsage(writer: *Io.Writer) noreturn {
+fn printUsage(writer: *Io.Writer, exit_code: u8) noreturn {
     const usage =
-        \\{s}Usage{s}: zig80_test [COMMAND] <OPTION>
+        \\{s}Usage{s}: zig80_test <command> [options]
         \\
         \\
     ;
@@ -31,26 +34,26 @@ fn printUsage(writer: *Io.Writer) noreturn {
         \\    {s}zex{s}         Run the Z80 instruction set exerciser test suite
         \\    {s}z80test{s}     Run the Zilog Z80 CPU test suite
         \\    
-        \\    {s}sst{s}:
-        \\        {s}-s{s}, {s}--summary{s}
-        \\                Only show the tests summary
-        \\        {s}-r{s}, {s}--run{s} <FILE>
-        \\                Run tests for one opcode
+        \\{s}sst options{s}:
+        \\    {s}-s{s}, {s}--summary{s}
+        \\        Only show the test summary
+        \\    {s}-r{s}, {s}--run{s} <file>
+        \\        Run the specified opcode test file
         \\
-        \\    {s}zex{s}:
-        \\        {s}-r{s}, {s}--run{s} [zexall|zexdoc|prelim]
-        \\                Run the specified test rom
-        \\    
-        \\    {s}z80test{s}:
-        \\        {s}-r{s}, {s}--run{s} [ccf|doc|docflags|flags|full|memptr]
-        \\                Run the specified test rom
+        \\{s}zex options{s}:
+        \\    {s}-r{s}, {s}--run{s} <zexall|zexdoc|prelim>
+        \\        Run the specified test rom
+        \\
+        \\{s}z80test options{s}:
+        \\    {s}-r{s}, {s}--run{s} <ccf|doc|docflags|flags|full|memptr>
+        \\        Run the specified test rom
         \\
         \\
     ;
     const options =
-        \\{s}Options{s}:
+        \\{s}Global options{s}:
         \\    {s}-h{s}, {s}--help{s}
-        \\                Show this help message
+        \\        Show this help message
         \\
     ;
 
@@ -84,7 +87,7 @@ fn printUsage(writer: *Io.Writer) noreturn {
 
     writer.flush() catch {};
 
-    std.process.exit(1);
+    std.process.exit(exit_code);
 }
 
 fn runSST(io: Io, alloc: Allocator, writer: *Io.Writer, args: []const []const u8) !void {
@@ -93,39 +96,33 @@ fn runSST(io: Io, alloc: Allocator, writer: *Io.Writer, args: []const []const u8
         .summary_only = false,
     };
 
-    if (args.len == 3 and eql(u8, args[2], "-s") or eql(u8, args[2], "--summary")) {
+    if (args.len == 3 and (eql(u8, args[2], "-s") or eql(u8, args[2], "--summary"))) {
         config.summary_only = true;
-    } else if (args.len == 4 and eql(u8, args[2], "-r") or eql(u8, args[2], "--run")) {
+    } else if (args.len == 4 and (eql(u8, args[2], "-r") or eql(u8, args[2], "--run"))) {
         config.file_to_run = args[3];
     } else if (args.len != 2) {
-        printUsage(writer);
+        printUsage(writer, FAILURE);
     }
 
     try sst.run(io, alloc, writer, config);
 }
 
-fn runZEX(io: Io, alloc: Allocator, writer: *Io.Writer, args: []const []const u8) !void {
+fn runWithRom(
+    io: Io,
+    alloc: Allocator,
+    writer: *Io.Writer,
+    args: []const []const u8,
+    runner: anytype,
+) !void {
     var rom: ?[]const u8 = null;
 
-    if (args.len == 4 and eql(u8, args[2], "-r") or eql(u8, args[2], "--run")) {
+    if (args.len == 4 and (eql(u8, args[2], "-r") or eql(u8, args[2], "--run"))) {
         rom = args[3];
     } else if (args.len != 2) {
-        printUsage(writer);
+        printUsage(writer, FAILURE);
     }
 
-    try zex.run(io, alloc, writer, rom);
-}
-
-fn runZ80test(io: Io, alloc: Allocator, writer: *Io.Writer, args: []const []const u8) !void {
-    var rom: ?[]const u8 = null;
-
-    if (args.len == 4 and eql(u8, args[2], "-r") or eql(u8, args[2], "--run")) {
-        rom = args[3];
-    } else if (args.len != 2) {
-        printUsage(writer);
-    }
-
-    try z80test.run(io, alloc, writer, rom);
+    try runner.run(io, alloc, writer, rom);
 }
 
 // ********** public functions ********** //
@@ -141,7 +138,7 @@ pub fn main(init: std.process.Init) !void {
     const args = try init.minimal.args.toSlice(init.arena.allocator());
 
     if (args.len < 2 or args.len > 4) {
-        printUsage(stdout);
+        printUsage(stdout, FAILURE);
     }
 
     const command = args[1];
@@ -149,10 +146,12 @@ pub fn main(init: std.process.Init) !void {
     if (eql(u8, command, "sst")) {
         try runSST(io, alloc, stdout, args);
     } else if (eql(u8, command, "zex")) {
-        try runZEX(io, alloc, stdout, args);
+        try runWithRom(io, alloc, stdout, args, zex);
     } else if (eql(u8, command, "z80test")) {
-        try runZ80test(io, alloc, stdout, args);
+        try runWithRom(io, alloc, stdout, args, z80test);
+    } else if (eql(u8, command, "-h") or eql(u8, command, "--help")) {
+        printUsage(stdout, SUCCESS);
     } else {
-        printUsage(stdout);
+        printUsage(stdout, FAILURE);
     }
 }
